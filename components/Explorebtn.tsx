31596a58e5d2ef6dee4a4f4e5e6638fe475fd0a5
luffy <luffy@gmail.com>
2025-11-17 16:04:07 +0530

    commit2

---
+++ components/Explorebtn.tsx
@@ -0,0 +1,12 @@
+'use client'
+const Explorebtn = () => {
+    return (
+        <button type='button' id='explore-btn' className='mt-7 mx-auto' onClick={() => console.log("Clicked")}>
+            <a href='#events'>
+                Explore Events
+                <img src='/icons/arrow-down.svg' alt='arrow-down' width={24} height={24} />
+            </a>
+        </button>
+    )
+}
+export default Explorebtn
